384201159e9a5de6b8476cae4583d44dcd0d45a8
mohamed-1995-hassan <59643006+mohamed-1995-hassan@users.noreply.github.com>
2021-05-20 12:09:52 +0200

    Revert "filterpush"
    This reverts commit b2c11778e681538ffacc776ebc3646bcb6a450b8.

--- src/index.js
+++ src/index.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Filters from './Components/Filters/Filter';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
-import 'bootstrap/dist/css/bootstrap.min.css';
-
 
 ReactDOM.render(
   <React.StrictMode>
-    <Filters />
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
