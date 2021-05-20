b2c11778e681538ffacc776ebc3646bcb6a450b8
mohamed-1995-hassan <59643006+mohamed-1995-hassan@users.noreply.github.com>
2021-05-20 11:36:18 +0200

    filterpush
    filtercommit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Filters from './Components/Filters/Filter';
 import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Filters />
   </React.StrictMode>,
   document.getElementById('root')
 );
