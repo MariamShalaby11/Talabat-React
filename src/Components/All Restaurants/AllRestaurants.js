e32e2c673cd213ca7a8837f613b2a67eaefee01d
MariamShalaby <77920984+MariamShalaby11@users.noreply.github.com>
2021-06-11 23:16:04 +0200

    AllResturants

--- src/Components/All Restaurants/AllRestaurants.js
+++ src/Components/All Restaurants/AllRestaurants.js
@@ -8,65 +8,94 @@ import axios from 'axios';
 
 export default class AllRestaurants extends React.Component{
     state={
-        AllRestaurants:[],SearchTerm:""
+        AllRestaurants:[],SearchTerm:"",
+        Cusine:[]
 
+    }
+    ShowCusineNameHandler=(i)=>{
+        axios.get(`http://localhost:58160/api/cusine/${i}`).then(res=>{
+
+            this.setState({Cusine:res.data})
+    
+            console.log(res.data); 
+             
+            }) 
     }
   
     async componentDidMount(){
 
-        await axios.get('http://localhost:58160/api/AllRestPage').then(res=>{
+        await axios.get(`http://localhost:58160/api/AllRestPage`).then(res=>{
   
             this.setState({AllRestaurants:res.data})
   
            console.log(res.data)  
-             }) 
-            }      
+             })
+
+
+            //  this.ShowCusineNameHandler();
+             {this.state.AllRestaurants.map((AllRest)=>{
+                this.ShowCusineNameHandler(AllRest.RestaurantId)
+             })}
+
+            //  await axios.get(`http://localhost:58160/api/cusine/${this.state.RestId}`).then(res=>{
+  
+            //     this.setState({Cusine:res.data})
+      
+            //    console.log(res.data)  
+            //     }) 
+            }     
+             
     render(){
         return(
-        <div class="container">
-            <div class="row">
-                <div class="card bg-transparent" id="card1">
-                    <div class="card-header" style={{height: 50}} id="TopCard">
+        <div className="container">
+            <div className="row">
+                <div className="card bg-transparent" id="card1">
+                    <div className="card-header" style={{height: 50}} id="TopCard">
                       <h4>All Restaurants</h4>
                     </div>
-                    <div class="card-body bg-transparent">
-                        <div class="form-inline my-2 my-lg-0">
-                            <div class="input-group input-group" id="myDiv">
-                                    <div class="input-group-prepend ">
-                                        <span class="input-group-text bg-transparent" style={{borderRight:"none"}}  id="MySearchIcon">
-                                            <FaSearch/>
-                                            </span>   
-                                    </div>                             
-                                <input type="text" class="form-control" aria-label="Small"
-                                    aria-describedby="inputGroup-sizing-sm" placeholder="Search Restaurant"   onChange={(e) => {
 
-                                        if (e.target.value != '') {
-                                            axios.get(`http://localhost:58160/api/search/${e.target.value}`).then(
-                                                (res) => {
-                                                    this.state.AllRestaurants = res.data
-                                                    this.setState({
-                                                        AllRestaurants: this.state.AllRestaurants
-                                                    })
-                                                }
-                                            )
-                                        }
-                                        else{
-                                            this.componentDidMount()
-                                        } 
-    
-                                    }}id="MySearchBox"  style={{borderLeft:"none"}}/>                                                                                                                                      
-                            </div>                          
+                    <div class="col-lg-3 col-md-12" style={{ marginLeft: 5 }}>
+                        <div class="form-group has-search searchBox" style={{ marginTop: 20 }}>
+                            <span class="fa fa-search form-control-feedback srchIcon"><FaSearch/></span>
+                            <input type="text" class="form-control formCtrl" placeholder="Search Restaurant"
+
+                                onChange={(e) => {
+
+                                    if (e.target.value != '') {
+                                        axios.get(`http://localhost:58160/api/search/${e.target.value}`).then(
+                                            (res) => {
+                                                this.state.AllRestaurants = res.data
+                                                this.setState({
+                                                    AllRestaurants: this.state.AllRestaurants
+                                                })
+                                            }
+                                        )
+                                    }
+                                    else{
+                                        this.componentDidMount()
+                                    } 
+
+                                }}
+                            />
                         </div>
+                        </div>                   
 
-                            <div class="row special-list" id="rest">
+                    <div className="card-body bg-transparent">                                              
+                            <div className="row special-list" id="rest">
                                 {this.state.AllRestaurants.map((AllRest)=>{
+                                    // {this.setState({RestId:AllRest.RestaurantId})}
                                     return(
-                                <div class="col-lg-4 col-md-6">
-                                    <div class="gallery-single fix">
-                                        <img src={slide1} class="img-fluid" alt="Image" id="RestImage"/>
-                                        <div class="card-title">
+                                <div className="col-lg-4 col-md-6">
+                                    <div className="gallery-single fix">
+                                        <img src={slide1} className="img-fluid" alt="Image" id="RestImage"/>
+                                        <div className="card-title">
                                             <h4>{AllRest.RestaurantName}</h4>
-                                            <p>{AllRest.Description}</p>
+                                            {this.state.Cusine.map((cusObj)=>{
+                                                console.log(cusObj);
+                                            return(
+                                                <p>{cusObj.CuisineName}</p>
+                                            )
+                                        })}
                                         </div>
                                     </div>
                                 </div>   
@@ -78,22 +107,22 @@ export default class AllRestaurants extends React.Component{
 
            {/*  -----------------Pagination------------------------------ */}
 
-                    <div class="card-footer text-muted" id="MyCardFooter">
+                    <div className="card-footer text-muted" id="MyCardFooter">
                         <nav aria-label="Page navigation example">
-                            <ul class="pagination" id="MyPagination">
-                              <li class="page-item">
-                                <a class="page-link" href="#" aria-label="Previous">
+                            <ul className="pagination" id="MyPagination">
+                              <li className="page-item">
+                                <a className="page-link" href="#" aria-label="Previous" style={{color:'#810000'}}>
                                   <span aria-hidden="true">&laquo;</span>
                                 </a>
                               </li>
-                              <li class="page-item"><a class="page-link" href="#">1</a></li>
-                              <li class="page-item"><a class="page-link" href="#">2</a></li>
-                              <li class="page-item"><a class="page-link" href="#">3</a></li>
-                              <li class="page-item"><a class="page-link" href="#">4</a></li>
-                              <li class="page-item"><a class="page-link" href="#">5</a></li>
-                              <li class="page-item"><a class="page-link" href="#">6</a></li>
-                              <li class="page-item">
-                                <a class="page-link" href="#" aria-label="Next">
+                              <li className="page-item"><a className="page-link" href="#" style={{color:'#810000'}}>1</a></li>
+                              <li className="page-item"><a className="page-link" href="#" style={{color:'#810000'}}>2</a></li>
+                              <li className="page-item"><a className="page-link" href="#" style={{color:'#810000'}}>3</a></li>
+                              <li className="page-item"><a className="page-link" href="#" style={{color:'#810000'}}>4</a></li>
+                              <li className="page-item"><a className="page-link" href="#" style={{color:'#810000'}}>5</a></li>
+                              <li className="page-item"><a className="page-link" href="#" style={{color:'#810000'}}>6</a></li>
+                              <li className="page-item">
+                                <a className="page-link" href="#" aria-label="Next" style={{color:'#810000'}}>
                                   <span aria-hidden="true">&raquo;</span>
                                 </a>
                               </li>
